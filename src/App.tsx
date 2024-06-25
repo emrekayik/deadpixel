ba2ded04e09d9b2ef2346f4631abc4608db10379
emrekayik <emrekayik1905@gmail.com>
2024-06-26 02:45:27 +0300

    shadcn initialized

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-function App() {
+import { ThemeProvider } from '@/components/theme-provider'
+import { Header } from './components/global/header'
 
-  return (
-    <>
-    </>
-  )
+function App() {
+    return (
+        <>
+            <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+                <Header />
+            </ThemeProvider>
+        </>
+    )
 }
 
 export default App
---
+++ src/components/global/header.tsx
@@ -0,0 +1,10 @@
+import { ModeToggle } from '@/components/mode-toggle'
+
+export function Header() {
+    return (
+        <header>
+            <h1>Header</h1>
+            <ModeToggle />
+        </header>
+    )
+}
